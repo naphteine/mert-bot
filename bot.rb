012370912d3313923e0f3df76d548b4a95667f54
naphteine <58526116+naphteine@users.noreply.github.com>
2021-03-14 17:22:13 +0300

    Added Telegram exception handling and updated caricature code

--- bot.rb
+++ bot.rb
@@ -18,6 +18,8 @@ $states = {
 $dialog = JSON.load_file "assets/dialog.json"
 $responses = JSON.load_file "assets/answers.json"
 
+$caricatures = Dir.glob('assets/img/caricatures/*')
+
 # Functions
 def logger(text)
 	puts "#{DateTime.now} #{text}"
@@ -81,104 +83,109 @@ end
 logger("Buruki uyanıyor!")
 
 # Telegram loop
-Telegram::Bot::Client.run($token) do |bot|
-	# Scheduler
-	$scheduler.every '1685s' do
-		reply = ["Yine çok neşelisiniz. Yazsanıza aq", "Anlatın amk", "Saat #{DateTime.now.strftime("%H:%M")} olmuş, napıyorsunuz gençler"].sample
-		logger ">>> chat##{$master_chat_id}: #{reply}"
-		bot.api.send_message(chat_id:  $master_chat_id, text: reply)
-	end
+begin
+	Telegram::Bot::Client.run($token) do |bot|
+		# Scheduler
+		$scheduler.every '1685s' do
+			reply = ["Yine çok neşelisiniz amk yazın hadi", "Amına koyem yazın gençlik", "Yine çok neşelisiniz. Yazsanıza aq", "Anlatın amk", "Saat #{DateTime.now.strftime("%H:%M")} olmuş, napıyorsunuz gençler"].sample
+			logger ">>> chat##{$master_chat_id}: #{reply}"
+			bot.api.send_message(chat_id:  $master_chat_id, text: reply)
+		end
 
-	# Replies
-	bot.listen do |message|
-		case message
-		when Telegram::Bot::Types::InlineQuery
-			results = [
-				[1, 'Buruki', "Tek güç Buruki POWER!"],
-				[2, 'Mert', "Ne var lan"]
-			].map do |arr|
-				Telegram::Bot::Types::InlineQueryResultArticle.new(
-					id: arr[0],
-					title: arr[1],
-					input_message_content: Telegram::Bot::Types::InputTextMessageContent.new(message_text: arr[2])
-				)
-			end
-			bot.api.answer_inline_query(inline_query_id: message.id, results: results, cache_time: 5)
-			logger "InlineQuery activity!"
-		when Telegram::Bot::Types::Message
-			logger "chat##{message.chat.id} #{message.from.id}@#{message.from.username}: #{message.text}"
-			
-			case message.text
-			# Priority 1: Commands
-			when /^\/start$/i then reply = "Türkçe konuş"
-			when /^\/kafayı göm$/i
-				if message.from.id == $master_id
-					reply = "Gömemem be kardeş"
+		# Replies
+		bot.listen do |message|
+			case message
+			when Telegram::Bot::Types::InlineQuery
+				results = [
+					[1, 'Buruki', "Tek güç Buruki POWER!"],
+					[2, 'Mert', "Ne var lan"]
+				].map do |arr|
+					Telegram::Bot::Types::InlineQueryResultArticle.new(
+						id: arr[0],
+						title: arr[1],
+						input_message_content: Telegram::Bot::Types::InputTextMessageContent.new(message_text: arr[2])
+					)
 				end
-			when /^\/kafayı çek$/i
-				if message.from.id == $master_id
-					begin
-						$diyalog = JSON.load_file "assets/dialog.json"
-						$cevaplar = JSON.load_file "assets/answers.json"
-						reply = "Çektim çektim"
-					rescue Exception => e
-						reply = "Çekemedim"
+				bot.api.answer_inline_query(inline_query_id: message.id, results: results, cache_time: 5)
+				logger "InlineQuery activity!"
+			when Telegram::Bot::Types::Message
+				logger "chat##{message.chat.id} #{message.from.id}@#{message.from.username}: #{message.text}"
+				
+				case message.text
+				# Priority 1: Commands
+				when /^\/start$/i then reply = "Türkçe konuş"
+				when /^\/kafayı göm$/i
+					if message.from.id == $master_id
+						reply = "Gömemem be kardeş"
+					end
+				when /^\/kafayı çek$/i
+					if message.from.id == $master_id
+						begin
+							$diyalog = JSON.load_file "assets/dialog.json"
+							$cevaplar = JSON.load_file "assets/answers.json"
+							$caricatures = Dir.glob('assets/caricatures/*')
+							reply = "Çektim çektim"
+						rescue Exception => e
+							reply = "Çekemedim"
+						end
+					end
+
+				# Priority 2: Quick responses
+				when /^(Mert ibnesi)$|^(Amcık Mert)$|^(İbne Mert)$/i then reply = "Doğru konuş lan"
+				when /^(Melih ibnesi)$|^(Amcık Melih)$|^(İbne Melih)$/i then reply = "Şşş ibne olabilir ama o da içimizden"
+				when /^Use Signal$/i then reply = "I can't aq"
+				when /^Gönüller bir$/i then reply = "Tabbbe ✊✊"
+				when /^Ihı ohı ohı Mert$/i then reply = "Hıı şurdaki kızları ellesek"
+				when /^31 çek$/i then reply = "Sevisiyorum ben düzenli"
+				when /^Sana girsin$/i then reply = "Sana da " + File.readlines("assets/sokulabilir").sample.strip.downcase + " girsin"
+				when /^Mert Kore'de saat kaç$/i then reply = "Kardeş Kore şuan " + DateTime.now.new_offset('+09:00').strftime("%H:%M")
+				when /^Mert isim salla$/i then reply = File.readlines("assets/isimler").sample.strip.capitalize + " nasıl"
+				when /(görüyon mu)$|(görüyor musun)$/i then reply = geri_sok(message.text) + " sana girsin"
+				when /^(İyi geceler Mert)$|^(İyi geceler)$|^(İyi geceler beyler)$/i then reply = "İyi geceler kardeşim"
+				when /^(Selam Mert)$|^(Selamlar)$|^(Selam beyler)$|^(Merhaba beyler)$|^(Merhaba Mert)$|^(Merhaba)$/i then reply = "Hoş geldin kardeş"
+				when /^(Bak)$|^(\(o\)\)\))$/i then reply = "(o)))"
+				when /^(Görüşürüz Mert)$|^(Mert görüşürüz)$|^(Görüşürüz beyler)$/i then reply = "Görüşürüz kardeşim"
+				when /\b(Maya'yı sik)$|\b(Mayayı sik)$/i then reply = "Ne diyon lan aq Maya benim bacım sayılır. Seni sikerim doğru konuş"
+				when /^Mert, Melih'i sik$/i then reply = "Meliiih gel attaya gidecez"
+				when /^Sinirim çok bozuk$/i then reply = "Sinirlerini topla kardeş"
+				when /^Yarın buluşalım mı$/i then reply = "Buluşalım ben de geliyorum"
+				when /^Yarın erken kalkacağım$/i then reply = "Git yat uyu o zaman"
+				when /^Yazılım$/i then reply = "Yazılmayalım"
+				when /^Seni seviyorum kral$/i then reply = "Eyvallah tosun ben de seni seviyim"
+				when /^En iyi dostumsun$/i then reply = "Sen benim kardeşimsin kardeşim. Ölümüne"
+				when /^Hastayım$/i then reply = "Geçmiş olsun kardeşim"
+				when /^Mert'e vurdururuz$/i then reply = "Kim bana vurduruyor şimdi ona göre şeyetcem"
+				when /^Mert neyin var$/i then reply = "Bir şeyim yok Allaha şükür jsadhfas"
+				when /^Görmek isterim$/i then reply = "Ezan bitsin hemen gösterecem"
+				when /^Adamsın lan Mert$/i then reply = "Eyvallah kardeşim"
+				when /^👊$/i then reply = "👊🏽"
+				when /(Canım sıkılıyor)$|(canım sıkıldı)$/i
+					bot.api.send_photo(chat_id: message.chat.id, photo: Faraday::UploadIO.new($caricatures.sample, 'image/jpeg'), reply_to_message_id: message.message_id)
+				when /([asdfghjklşi]){6}\w+/i then reply = ["dkajflaskdjf", "kjdsalfjaldksfjalk", "sdkjlsdfjl", "dsaşfkjsaldf", "sakjdkasjd", "dsşafjasdkfs"].sample
+
+				# Priority 3: Dialog system
+				else
+					reply = diyalog_kur(message.from.id, message.text)
+				end
+
+				# Priority 4: Words
+				if reply.to_s.strip.empty?
+					case message.text
+					when /\bMert\b/i then reply = ["Adım geçti sanki lan", "Şşt arkamdan konuşmayın"].sample
+					when /\bAm\b/i then reply = "Lam kim dedi onu nerede"
 					end
 				end
 
-			# Priority 2: Quick responses
-			when /^(Mert ibnesi)$|^(Amcık Mert)$|^(İbne Mert)$/i then reply = "Doğru konuş lan"
-			when /^(Melih ibnesi)$|^(Amcık Melih)$|^(İbne Melih)$/i then reply = "Şşş ibne olabilir ama o da içimizden"
-			when /^Use Signal$/i then reply = "I can't aq"
-			when /^Gönüller bir$/i then reply = "Tabbbe ✊✊"
-			when /^Ihı ohı ohı Mert$/i then reply = "Hıı şurdaki kızları ellesek"
-			when /^31 çek$/i then reply = "Sevisiyorum ben düzenli"
-			when /^Sana girsin$/i then reply = "Sana da " + File.readlines("assets/sokulabilir").sample.strip.downcase + " girsin"
-			when /^Mert Kore'de saat kaç$/i then reply = "Kardeş Kore şuan " + DateTime.now.new_offset('+09:00').strftime("%H:%M")
-			when /^Mert isim salla$/i then reply = File.readlines("assets/isimler").sample.strip.capitalize + " nasıl"
-			when /(görüyon mu)$|(görüyor musun)$/i then reply = geri_sok(message.text) + " sana girsin"
-			when /^(İyi geceler Mert)$|^(İyi geceler)$|^(İyi geceler beyler)$/i then reply = "İyi geceler kardeşim"
-			when /^(Selam Mert)$|^(Selamlar)$|^(Selam beyler)$|^(Merhaba beyler)$|^(Merhaba Mert)$|^(Merhaba)$/i then reply = "Hoş geldin kardeş"
-			when /^(Bak)$|^(\(o\)\)\))$/i then reply = "(o)))"
-			when /^(Görüşürüz Mert)$|^(Mert görüşürüz)$|^(Görüşürüz beyler)$/i then reply = "Görüşürüz kardeşim"
-			when /\b(Maya'yı sik)$|\b(Mayayı sik)$/i then reply = "Ne diyon lan aq Maya benim bacım sayılır. Seni sikerim doğru konuş"
-			when /^Mert, Melih'i sik$/i then reply = "Meliiih gel attaya gidecez"
-			when /^Sinirim çok bozuk$/i then reply = "Sinirlerini topla kardeş"
-			when /^Yarın buluşalım mı$/i then reply = "Buluşalım ben de geliyorum"
-			when /^Yarın erken kalkacağım$/i then reply = "Git yat uyu o zaman"
-			when /^Yazılım$/i then reply = "Yazılmayalım"
-			when /^Seni seviyorum kral$/i then reply = "Eyvallah tosun ben de seni seviyim"
-			when /^En iyi dostumsun$/i then reply = "Sen benim kardeşimsin kardeşim. Ölümüne"
-			when /^Hastayım$/i then reply = "Geçmiş olsun kardeşim"
-			when /^Mert'e vurdururuz$/i then reply = "Kim bana vurduruyor şimdi ona göre şeyetcem"
-			when /^Mert neyin var$/i then reply = "Bir şeyim yok Allaha şükür jsadhfas"
-			when /^Görmek isterim$/i then reply = "Ezan bitsin hemen gösterecem"
-			when /^Adamsın lan Mert$/i then reply = "Eyvallah kardeşim"
-			when /^👊$/i then reply = "👊🏽"
-			when /(Canım sıkılıyor)$|(canım sıkıldı)$/i
-				bot.api.send_photo(chat_id: message.chat.id, photo: Faraday::UploadIO.new('assets/images/Kurtuluş.jpg', 'image/jpeg'), reply_to_message_id: message.message_id)
-			when /([asdfghjklşi]){6}\w+/i then reply = ["dkajflaskdjf", "kjdsalfjaldksfjalk", "sdkjlsdfjl", "dsaşfkjsaldf", "sakjdkasjd", "dsşafjasdkfs"].sample
-
-			# Priority 3: Dialog system
-			else
-				reply = diyalog_kur(message.from.id, message.text)
-			end
-
-			# Priority 4: Words
-			if reply.to_s.strip.empty?
-				case message.text
-				when /\bMert\b/i then reply = ["Adım geçti sanki lan", "Şşt arkamdan konuşmayın"].sample
-				when /\bAm\b/i then reply = "Lam kim dedi onu nerede"
+				# Send the message, if it exists
+				unless reply.to_s.strip.empty?
+					logger ">>> chat##{message.chat.id} #{message.from.id}@#{message.from.username}: #{reply}"
+					bot.api.send_message(chat_id: message.chat.id, text: reply)
 				end
-			end
-
-			# Send the message, if it exists
-			unless reply.to_s.strip.empty?
-				logger ">>> chat##{message.chat.id} #{message.from.id}@#{message.from.username}: #{reply}"
-				bot.api.send_message(chat_id: message.chat.id, text: reply)
 			end
 		end
 	end
+rescue Exception => e
+	logger("EXCEPTION: #{e}")
 end
 
 logger("Buruki uyumaya gidiyor..")
